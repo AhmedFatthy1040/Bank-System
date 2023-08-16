66cc050c0719e0260484fc303fc12d3ea27d8740
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2023-08-16 07:16:47 +0300

    Added setDebitCard method in Checking.java class.

--- Checking.java
+++ Checking.java
@@ -3,9 +3,17 @@ public class Checking extends Account {
     int debitCardPIN;
     public Checking(String name, String SSN, double initDeposit) {
         super(name, SSN, initDeposit);
+        accountNumber = "2" + accountNumber;
+        setDebitCard();
     }
     public void showInfo() {
         super.showInfo();
-        System.out.println("Accout Type: Checking Account");
+        System.out.println(" Your Checking Account Features" +
+                "\n Debit Card Number: " + debitCardNumber +
+                "\n Debit Card PIN: " + debitCardPIN);
+    }
+    private void setDebitCard() {
+        debitCardNumber = (int)(Math.random() * Math.pow(10, 12));
+        debitCardPIN = (int)(Math.random() * Math.pow(10, 4));
     }
 }
